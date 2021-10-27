d2e240b1ce30c4933e497df4ec4063bc49bb4f2e
little weak_duck <83490374+little-weakduck@users.noreply.github.com>
2021-10-28 02:08:27 +0800

    声音全部添加完成（待调整音量

--- people/js/people.js
+++ people/js/people.js
@@ -1,13 +1,32 @@
-// function playIt()
-// {
-//    document.getElementById("peoplePic").innerHTML="<embed src='people/css/123.mp3' autostart=true loop=false volume=100 hidden=true>";
-//    return true;
-// }
-
-var toyAudio = new Audio ('css/123.mp3') 
-
-function playIt()
+//导入音频
+var toyAudio = new Audio ('css/toyStory.mp3') ;
+var monsterAudio = new Audio ('css/monster.mp3') ;
+var seaAudio = new Audio ('css/sea.mp3') ;
+var carAudio = new Audio ('css/car.mp3') ;
+var cocoAudio = new Audio ('css/coco.mp3') ;
+var IncrediblesAudio = new Audio ('css/Incredibles.mp3') ;
+//播放
+function playToy()
 {
-    toyAudio.loop = false;
     toyAudio.play();
+}
+function playMonster()
+{
+    monsterAudio.play();
+}
+function playSea()
+{
+    seaAudio.play();
+}
+function playCar()
+{
+    carAudio.play();
+}
+function playCoco()
+{
+    cocoAudio.play();
+}
+function playIncredibles()
+{
+    IncrediblesAudio.play();
 }
