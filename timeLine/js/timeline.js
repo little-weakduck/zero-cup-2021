1a8708eddd6cbcc65a92c7705a61defd67490ba6
Muyuansa <2561520653@qq.com>
2021-11-04 20:49:11 +0800

    timeline update

---
+++ timeLine/js/timeline.js
@@ -0,0 +1,13 @@
+const width=$(window).width();
+$(document).ready(function(){
+    let currentscroll=0;
+	$('#right').click(function(){
+        currentscroll=currentscroll+width;
+		$(window).scrollLeft(currentscroll);
+
+	});
+	$('#left').click(function(){
+        currentscroll=currentscroll-width;
+		$(window).scrollLeft(currentscroll);
+	});
+});
