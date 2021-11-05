dd774b113496ccbcbfd1bfb44b2645f313a6dfe8
little weak_duck <83490374+little-weakduck@users.noreply.github.com>
2021-11-05 12:18:27 +0800

    dot 1.1

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 function jumpHistory() {
   window.location.hash = "#history";
+  document.getElementById("w").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("q").style.backgroundColor = "snow";
+  document.getElementById("e").style.backgroundColor = "snow";
+  document.getElementById("r").style.backgroundColor = "snow";
+  document.getElementById("t").style.backgroundColor = "snow";
+  document.getElementById("y").style.backgroundColor = "snow";
 }
+
 function jumpFrist() {
   window.location.hash = "#first";
+  document.getElementById("q").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("w").style.backgroundColor = "snow";
+  document.getElementById("e").style.backgroundColor = "snow";
+  document.getElementById("r").style.backgroundColor = "snow";
+  document.getElementById("t").style.backgroundColor = "snow";
+  document.getElementById("y").style.backgroundColor = "snow";
 }
+
 function jumpFilm() {
   window.location.hash = "#film";
+  document.getElementById("e").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("q").style.backgroundColor = "snow";
+  document.getElementById("w").style.backgroundColor = "snow";
+  document.getElementById("r").style.backgroundColor = "snow";
+  document.getElementById("t").style.backgroundColor = "snow";
+  document.getElementById("y").style.backgroundColor = "snow";
 }
+
 function jumpPeople() {
   window.location.hash = "#people";
+  document.getElementById("r").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("q").style.backgroundColor = "snow";
+  document.getElementById("e").style.backgroundColor = "snow";
+  document.getElementById("w").style.backgroundColor = "snow";
+  document.getElementById("t").style.backgroundColor = "snow";
+  document.getElementById("y").style.backgroundColor = "snow";
 }
+
 function jumpAward() {
   window.location.hash = "#award";
+  document.getElementById("t").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("q").style.backgroundColor = "snow";
+  document.getElementById("e").style.backgroundColor = "snow";
+  document.getElementById("r").style.backgroundColor = "snow";
+  document.getElementById("w").style.backgroundColor = "snow";
+  document.getElementById("y").style.backgroundColor = "snow";
 }
+
 function jumpEnd() {
   window.location.hash = "#end";
+  document.getElementById("y").style.backgroundColor = "rgb(161, 171, 238)";
+  document.getElementById("q").style.backgroundColor = "snow";
+  document.getElementById("e").style.backgroundColor = "snow";
+  document.getElementById("r").style.backgroundColor = "snow";
+  document.getElementById("t").style.backgroundColor = "snow";
+  document.getElementById("w").style.backgroundColor = "snow";
 }
