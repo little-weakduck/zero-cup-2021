126c20dc4aac80a84400d8f7a58a1e7b4fa9dc07
little weak_duck <83490374+little-weakduck@users.noreply.github.com>
2021-11-10 10:41:46 +0800

    导航栏完成

--- index.js
+++ index.js
@@ -1,59 +1,89 @@
 function jumpHistory() {
   window.location.hash = "#history";
-  document.getElementById("w").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("q").style.backgroundColor = "snow";
-  document.getElementById("e").style.backgroundColor = "snow";
-  document.getElementById("r").style.backgroundColor = "snow";
-  document.getElementById("t").style.backgroundColor = "snow";
-  document.getElementById("y").style.backgroundColor = "snow";
 }
 
 function jumpFrist() {
   window.location.hash = "#first";
-  document.getElementById("q").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("w").style.backgroundColor = "snow";
-  document.getElementById("e").style.backgroundColor = "snow";
-  document.getElementById("r").style.backgroundColor = "snow";
-  document.getElementById("t").style.backgroundColor = "snow";
-  document.getElementById("y").style.backgroundColor = "snow";
 }
 
 function jumpFilm() {
   window.location.hash = "#film";
-  document.getElementById("e").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("q").style.backgroundColor = "snow";
-  document.getElementById("w").style.backgroundColor = "snow";
-  document.getElementById("r").style.backgroundColor = "snow";
-  document.getElementById("t").style.backgroundColor = "snow";
-  document.getElementById("y").style.backgroundColor = "snow";
 }
 
 function jumpPeople() {
   window.location.hash = "#people";
-  document.getElementById("r").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("q").style.backgroundColor = "snow";
-  document.getElementById("e").style.backgroundColor = "snow";
-  document.getElementById("w").style.backgroundColor = "snow";
-  document.getElementById("t").style.backgroundColor = "snow";
-  document.getElementById("y").style.backgroundColor = "snow";
 }
 
 function jumpAward() {
   window.location.hash = "#award";
-  document.getElementById("t").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("q").style.backgroundColor = "snow";
-  document.getElementById("e").style.backgroundColor = "snow";
-  document.getElementById("r").style.backgroundColor = "snow";
-  document.getElementById("w").style.backgroundColor = "snow";
-  document.getElementById("y").style.backgroundColor = "snow";
 }
 
 function jumpEnd() {
   window.location.hash = "#end";
-  document.getElementById("y").style.backgroundColor = "rgb(161, 171, 238)";
-  document.getElementById("q").style.backgroundColor = "snow";
-  document.getElementById("e").style.backgroundColor = "snow";
-  document.getElementById("r").style.backgroundColor = "snow";
-  document.getElementById("t").style.backgroundColor = "snow";
-  document.getElementById("w").style.backgroundColor = "snow";
 }
+
+var h = window.innerHeight;
+
+function pilot() {
+  if ($("html, body").scrollTop() < 0.5 * h) {
+    document.getElementById("q").style.backgroundColor = "rgb(161, 171, 238)";
+    document.getElementById("w").style.backgroundColor = "snow";
+    document.getElementById("e").style.backgroundColor = "snow";
+    document.getElementById("r").style.backgroundColor = "snow";
+    document.getElementById("t").style.backgroundColor = "snow";
+    document.getElementById("y").style.backgroundColor = "snow";
+  } else {
+    if ($("html, body").scrollTop() < 1.5 * h) {
+      document.getElementById("w").style.backgroundColor = "rgb(161, 171, 238)";
+      document.getElementById("q").style.backgroundColor = "snow";
+      document.getElementById("e").style.backgroundColor = "snow";
+      document.getElementById("r").style.backgroundColor = "snow";
+      document.getElementById("t").style.backgroundColor = "snow";
+      document.getElementById("y").style.backgroundColor = "snow";
+    } else {
+      if ($("html, body").scrollTop() < 2.5 * h) {
+        document.getElementById("e").style.backgroundColor =
+          "rgb(161, 171, 238)";
+        document.getElementById("q").style.backgroundColor = "snow";
+        document.getElementById("w").style.backgroundColor = "snow";
+        document.getElementById("r").style.backgroundColor = "snow";
+        document.getElementById("t").style.backgroundColor = "snow";
+        document.getElementById("y").style.backgroundColor = "snow";
+      } else {
+        if ($("html, body").scrollTop() < 3.5 * h) {
+          document.getElementById("r").style.backgroundColor =
+            "rgb(161, 171, 238)";
+          document.getElementById("q").style.backgroundColor = "snow";
+          document.getElementById("e").style.backgroundColor = "snow";
+          document.getElementById("w").style.backgroundColor = "snow";
+          document.getElementById("t").style.backgroundColor = "snow";
+          document.getElementById("y").style.backgroundColor = "snow";
+        } else {
+          if ($("html, body").scrollTop() < 4.5 * h) {
+            document.getElementById("t").style.backgroundColor =
+              "rgb(161, 171, 238)";
+            document.getElementById("q").style.backgroundColor = "snow";
+            document.getElementById("e").style.backgroundColor = "snow";
+            document.getElementById("r").style.backgroundColor = "snow";
+            document.getElementById("w").style.backgroundColor = "snow";
+            document.getElementById("y").style.backgroundColor = "snow";
+          } else {
+            document.getElementById("y").style.backgroundColor =
+              "rgb(161, 171, 238)";
+            document.getElementById("q").style.backgroundColor = "snow";
+            document.getElementById("e").style.backgroundColor = "snow";
+            document.getElementById("r").style.backgroundColor = "snow";
+            document.getElementById("t").style.backgroundColor = "snow";
+            document.getElementById("w").style.backgroundColor = "snow";
+          }
+        }
+      }
+    }
+  }
+}
+
+$(window).scroll(function () {
+  pilot();
+});
+
+window.onload = pilot();
